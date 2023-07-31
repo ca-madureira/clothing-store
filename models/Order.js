b146a921f9f67c95f821d2e2691c9d70ff99b376
camila-madureira <121184472+ca-madureira@users.noreply.github.com>
2023-07-30 22:29:21 -0300

    feat: create user and order model

---
+++ models/Order.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+  orderId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  products: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderModel = mongoose.model('Order', orderSchema);
+
+export default orderModel;
